354b581c2b115ba99bd61157078b64882a69bfe4
최원영 <choewy32@gmail.com>
2026-06-30 17:24:39 +0900

    refactor: AppConfig 리팩토링

--- src/main/java/lecture/core/AppConfig.java
+++ src/main/java/lecture/core/AppConfig.java
@@ -1,6 +1,8 @@
 package lecture.core;
 
+import lecture.core.discount.DiscountPolicy;
 import lecture.core.discount.RateDiscountPolicy;
+import lecture.core.member.MemberRepository;
 import lecture.core.member.MemberService;
 import lecture.core.member.MemberServiceImpl;
 import lecture.core.member.MemoryMemberRepository;
@@ -8,11 +10,19 @@ import lecture.core.order.OrderService;
 import lecture.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
